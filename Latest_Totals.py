5e4736111c6cc3e1ffda00eb46d4ba5f987a8064
facilius-natans <facilius.natans@gmail.com>
2020-04-12 17:16:44 -0400

    Two plots

--- Latest_Totals.py
+++ Latest_Totals.py
@@ -3,5 +3,5 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 df = pd.read_csv('Ontario_Totals.csv')
 _= sns.swarmplot(x='Category', y='Numbers', hue='Date', data=df)
-plt.title('Ontario COVID-19 cases in April 2020')
-plt.show()
+plt.title('Ontario COVID-19 cases in April 2020') 
+plt.show() 
