58bec4469f963a7e75d350870103a9f1cb94f6bd
facilius-natans <facilius.natans@gmail.com>
2020-04-12 15:31:43 -0400

    Total number of cases in April by categories

---
+++ Latest_Totals.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+df = pd.read_csv('Ontario_Totals.csv')
+_= sns.swarmplot(x='Category', y='Numbers', hue='Date', data=df)
+plt.title('Ontario COVID-19 cases in April 2020')
+plt.show()
